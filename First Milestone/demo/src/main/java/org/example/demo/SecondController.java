5453406c4aa6c2ef4f2058160b6c167cb4be1323
muhammedkh45 <127145833+muhammedkh45@users.noreply.github.com>
2024-11-24 21:47:24 +0200

    2nd commit

--- First Milestone/demo/src/main/java/org/example/demo/SecondController.java
+++ First Milestone/demo/src/main/java/org/example/demo/SecondController.java
@@ -2,8 +2,12 @@ package org.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import java.io.IOException;
 
-public class SecondController {
+
+public class SecondController1 {
+    HelloController objectController = new HelloController();
     @FXML
     private Button runButton;
 
@@ -18,5 +22,18 @@ public class SecondController {
     @FXML
     private void onStopButtonClick() {
         // Your stop code here
+    }
+    @FXML
+    private  void initialize() throws IOException {
+       try {
+           TextArea textArea = new TextArea();
+           textArea.setText(objectController.getContentofFile());
+           textArea.setWrapText(true);
+           textArea.setPrefRowCount(50);
+           textArea.setPrefColumnCount(50);
+       }catch (IOException e){
+           e.printStackTrace();;
+
+       }
     }
 }
