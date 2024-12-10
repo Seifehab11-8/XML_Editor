6022eb9c5d1ff155aa8808cd3ebe51c3d54f9835
Abdelrahmant26 <abdelrahmantaha26@gmail.com>
2024-12-10 18:59:15 +0200

    Removed useless comments

--- First Milestone/Prettifier/HelloWorld.java
+++ First Milestone/Prettifier/HelloWorld.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-
-// Main class
 public class HelloWorld {
     static String indent(int num){
         String ind = "";
@@ -17,15 +15,10 @@ public class HelloWorld {
         String out = "";
         int depth = 0;
         try (Scanner scanner = new Scanner(file)) {
-            // Read the file line by line
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                //System.out.println(line[1:line.length()-1]);
-                //stack.push(line);
-                //line = line.replaceAll("ro+t", "toor");
                 if(line.matches("^<(\\p{Alnum}+)>$")){
                     String key = line.replaceAll("(<?>?)", "");
-                    //System.out.println("start : "+ key);
                     stack.push(key);
                     out+=indent(depth)+line+"\n";
                     System.out.println(indent(depth)+line);
@@ -34,13 +27,10 @@ public class HelloWorld {
                 }
                 else if(line.matches("^</(\\p{Alnum}+)>$")){
                     String key = line.replaceAll("(<?>?)", "");
-                    //System.out.println("end : "+key);
                     depth--;
                     if(stack.peek()==key){
-                        //System.out.println("indeed they are equal");
                         stack.pop();
                     }else{
-                        //System.out.println("Not equal");
                     }
                     out+=indent(depth)+line+"\n";
                     System.out.println(indent(depth)+line);
@@ -48,7 +38,7 @@ public class HelloWorld {
                 }
                 else if(line.matches("<(\\p{Alnum}+)>\\p{ASCII}*<(/\\1)>")){
                     String indent = indent(depth);
-                    String indentplus = indent+"\t";//indent(depth)+"\t";
+                    String indentplus = indent+"\t";
                     line = line.replaceAll("<(\\p{Alnum}+)>(\\p{ASCII}*)</\\1>","<$1>\n"+indentplus+"$2\n"+indent+"<$1>");
                     out+=indent+line+"\n";
                 }
